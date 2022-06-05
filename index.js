854c399c7a0a0b44bdacec8a77b8349d09d10f59
Brian Kimutai <brian.kimutai@student.moringaschool.com>
2022-06-05 20:55:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+let employee={name:"brian",streetAddress:"Nairobi"};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    var newEmployee = Object.assign({}, employee, {[key]: value});
+    return newEmployee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    var newEmployee = Object.assign({}, employee);
+    delete newEmployee[key];
+    return newEmployee;
+
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+  return employee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+  return employee;
+}
